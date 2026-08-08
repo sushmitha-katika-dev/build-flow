d249a2b36a87d17a97538fcf6b1e1e324501956b
sushmitha-katika-dev <katikasushmitha7228@gmail.com>
2026-08-08 21:05:03 +0530

    chore: remove workforce-service

--- backend/workforce-service/src/main/java/com/buildflow/workforce/enums/AttendanceStatus.java
+++
@@ -1,7 +0,0 @@
-package com.buildflow.workforce.enums;
-
-public enum AttendanceStatus {
-    PRESENT,
-    ABSENT,
-    HALF_DAY
-}
--- backend/workforce-service/src/main/java/com/buildflow/workforce/enums/LabourStatus.java
+++
@@ -1,7 +0,0 @@
-package com.buildflow.workforce.enums;
-
-public enum LabourStatus {
-    ACTIVE,
-    ON_LEAVE,
-    INACTIVE
-}
