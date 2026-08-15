e0c6a2eb99673fe038a954628c4bbec7fbc15d40
sushmitha-katika-dev <katikasushmitha7228@gmail.com>
2026-08-15 22:37:48 +0530

    chore: remove unwanted code or clean code

--- run-tests.ps1
+++
@@ -1,8 +0,0 @@
-$services = Get-ChildItem -Path "backend" -Directory
-
-foreach ($service in $services) {
-    Write-Host "--- Testing $($service.Name) ---"
-    Push-Location $service.FullName
-    mvn test
-    Pop-Location
-}
